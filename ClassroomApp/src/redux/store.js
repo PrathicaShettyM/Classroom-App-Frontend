9e4c5718aa5047b7072c230ac7b88eb2f71a72fb
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-12-30 14:52:26 +0530

    added course slice to redux

--- ClassroomApp/src/redux/store.js
+++ ClassroomApp/src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit"
 
 import authReducer from './slices/authSlice';
+import courseReducer from './slices/courseSlice'
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
+        course: courseReducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
+    devTools: true
 });
 
 export default store;
