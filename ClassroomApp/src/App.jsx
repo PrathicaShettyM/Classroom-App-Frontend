e8f9a615282f962c8c40884fa8da186f2c611d33
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-12-30 11:57:00 +0530

    setting up redux tool kit

--- ClassroomApp/src/App.jsx
+++ ClassroomApp/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 
-import Footer from '../src/components/Footer'
 function App() {
 
   return (
       <div>
         hello
-        <Footer/>
       </div>
   )
 }
--- ClassroomApp/src/redux/store.js
+++ ClassroomApp/src/redux/store.js
@@ -1,15 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 
 import authReducer from './slices/authSlice';
-import courseReducer from "./slices/courseSlices";
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
-        course: courseReducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false}),
-    devTools: true
 });
 
 export default store;
