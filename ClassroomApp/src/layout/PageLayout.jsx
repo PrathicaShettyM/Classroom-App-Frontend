23beb808f90f5aeaaebdb393f00e0c765e967105
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2023-12-30 13:05:01 +0530

    added login and signup buttons

--- ClassroomApp/src/layout/PageLayout.jsx
+++ ClassroomApp/src/layout/PageLayout.jsx
@@ -17,13 +17,20 @@ function PageLayout({ children }){
         const drawerSide = document.getElementsByClassName("drawer-side");
         drawerSide[0].style.width = 'auto';
     }
-    
+
     function hideDrawer(){
         const element = document.getElementsByClassName("drawer-toggle");
         element[0].checked = false;
 
         const drawerSide = document.getElementsByClassName("drawer-side");
         drawerSide[0].style.width = 'auto';
+    }
+
+    function onLogout(e){
+        // todo
+        e.preventDefault();
+        // todo
+        navigate("/");
     }
 
     return(
@@ -35,18 +42,50 @@ function PageLayout({ children }){
                         <FiMenu onClick={changeWidth} size={"40px"} className="font-bold text-white m-4 border-2 p-1 border-white rounded"/>
                     </label>
                 </div>
-                <div className="drawer-side w-0 bg-teal-400">
-                    <label htmlFor="my-drawer" className="drawer-overlay bg-teal-400"></label>
-                    <ul className="menu p-4 w-48 sm:w-80 text-white bg-teal-400 relative">
+                <div className="drawer-side w-0">
+                    <label htmlFor="my-drawer" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-48 sm:w-80 text-white bg-teal-900 relative">
                         <li className="w-fit absolute right-2 z-50">
                             <button onClick={hideDrawer}>
                                 <AiFillCloseCircle size={24} />
                             </button>
                         </li>
                         <li><Link to="/">Home</Link></li>
+                        {
+                            isLoggedIn && role === "ADMIN" && (
+                                <li>
+                                    <Link to="/admin/dashboard">Admin Dashboard</Link>
+                                </li>
+                            )
+                        }
                         <li><Link to="/about">About Us</Link></li>
                         <li><Link to="/contact">Contact Us</Link></li>
                         <li><Link to="/courses">Courses</Link></li>
+                        {
+                            !isLoggedIn ? (
+                                <li className="absolute bottom-4 w-[90%]">
+                                    <div className="w-full flex items-center justify-center">
+                                        <button className="bg-green-500 text-white px-4 py-1 font-semibold rounded-md w-full">
+                                            <Link to="/login">Login</Link>
+                                        </button>
+                                        <button className="bg-pink-600 text-white px-4 py-1 font-semibold rounded-md w-full">
+                                            <Link to="/signup">Signup</Link>
+                                        </button>
+                                    </div>
+                                </li>
+                            ) : (
+                                <li className="absolute bottom-4 w-[90%]">
+                                    <div className="w-full flex items-center justify-center">
+                                        <button className="bg-blue-700 text-white px-4 py-1 font-semibold rounded-md w-full">
+                                            <Link to="/user/profile">Profile</Link>
+                                        </button>
+                                        <button className="bg-red-600 text-white px-4 py-1 font-semibold rounded-md w-full">
+                                            <Link onClick={onLogout}>Logout</Link>
+                                        </button>
+                                    </div>
+                                </li>
+                            )
+                        }
                     </ul>
                 </div>
             </div>
